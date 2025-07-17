a1247584fb36dcf4827c2bce64a5987e1ae1adf3
Danylo Liubota <daniil.luibota@gmail.com>
2025-07-17 19:39:58 +0300

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Container from "@/components/Container/Container";
+
+export default function Home() {
+  return (
+    <div>
+      <Container>
+        <h1>Hello</h1>
+      </Container>
+    </div>
+  );
+}
